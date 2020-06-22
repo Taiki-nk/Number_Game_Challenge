b15858037a0b048474a06a95b0b6b50556704809
Taiki-nk <taikifate.0206@gmail.com>
2020-06-23 06:07:19 +0900

    一通りゲーム機能は実装終了

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@
     constructor() {
       this.el = document.createElement("li");
       this.el.classList.add("pushed");
-      // this.el.textContent = "0";
 
       const board = document.getElementById("board");
       board.appendChild(this.el);
@@ -19,18 +18,23 @@
         this.el.classList.add("pushed");
         currentNum++;
       }
+      if(currentNum === 4){
+        clearTimeout(timeoutId);
+        startBtn.classList.remove('pushed');
+      }
     }
   }
   //ここまでPanelクラス
 
   class Board {
     constructor() {
-      this.nums = [0, 1, 2, 3];
       this.panels = [new Panel(), new Panel(), new Panel(), new Panel()];
 
     }
 
     showNumber(){
+      this.nums = [0, 1, 2, 3];
+
       this.panels.forEach((panel) => {
         panel.el.textContent = this.nums.splice(
           Math.floor(Math.random() * this.nums.length),
@@ -42,14 +46,29 @@
   }
   //ここまでBoardクラス
 
-  
+  function countUp(){
+    const timer = document.getElementById('timer');
+    timeoutId = setTimeout(() => {
+      timer.textContent = ((Date.now() - startTime)/1000).toFixed(2);
+      countUp();
+    },10);
 
-  let currentNum = 0;
+  }
+
+  let currentNum;
+  let startTime;
+  let timeoutId;
 
   const board = new Board()
 
   const startBtn = document.getElementById("start-btn");
   startBtn.addEventListener("click", () => {
+    startBtn.classList.add('pushed');
+    currentNum = 0;
+    startTime = Date.now()
     board.showNumber();
+    countUp();
   });
+
+
 }
