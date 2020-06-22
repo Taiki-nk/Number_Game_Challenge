6b0fe7efd9f7bd88b8b6aed4cfa19a8caeab7f15
Taiki-nk <taikifate.0206@gmail.com>
2020-06-23 00:15:44 +0900

    Boardクラス内にコンストラクタ、メソッド追加。startでランダムに数字を表示、順に幼いとクリックできない

--- js/main.js
+++ js/main.js
@@ -4,31 +4,52 @@
   class Panel {
     constructor() {
       this.el = document.createElement("li");
+      this.el.classList.add("pushed");
       // this.el.textContent = "0";
 
       const board = document.getElementById("board");
       board.appendChild(this.el);
+
+      this.el.addEventListener("click", () => {
+        this.startGame();
+      });
+    }
+    startGame() {
+      if (currentNum === parseInt(this.el.textContent)) {
+        this.el.classList.add("pushed");
+        currentNum++;
+      }
     }
   }
+  //ここまでPanelクラス
 
   class Board {
-    constructor(){
+    constructor() {
+      this.nums = [0, 1, 2, 3];
+      this.panels = [new Panel(), new Panel(), new Panel(), new Panel()];
 
     }
+
+    showNumber(){
+      this.panels.forEach((panel) => {
+        panel.el.textContent = this.nums.splice(
+          Math.floor(Math.random() * this.nums.length),
+          1
+        )[0];
+        panel.el.classList.remove("pushed");
+      });
+    }
   }
+  //ここまでBoardクラス
 
-  const nums = [0,1,2,3]
+  
 
-  const panels = [
-    new Panel(),
-    new Panel(),
-    new Panel(),
-    new Panel()
-  ];
+  let currentNum = 0;
 
-  panels.forEach(panel => {
-    //まだテスト中
-    panel.el.textContent = Math.floor(Math.random() * nums.length);;
+  const board = new Board()
+
+  const startBtn = document.getElementById("start-btn");
+  startBtn.addEventListener("click", () => {
+    board.showNumber();
   });
-
 }
