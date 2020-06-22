4bb949b8736363a02d44e1ffbab493f0bae3663c
Taiki-nk <taikifate.0206@gmail.com>
2020-06-23 07:10:33 +0900

    GameLevel変更中

--- js/main.js
+++ js/main.js
@@ -2,39 +2,60 @@
 
 {
   class Panel {
-    constructor() {
+    constructor(game) {
+      this.game = game
       this.el = document.createElement("li");
       this.el.classList.add("pushed");
 
       const board = document.getElementById("board");
       board.appendChild(this.el);
 
-      this.el.addEventListener("click", () => {
+      this.getEl().addEventListener("click", () => {
         this.startGame();
       });
     }
+
+    getEl(){
+      return this.el;
+    }
+
+    getCurrentNum(){
+      return this.game.currentNum;
+    }
+
+    addCurrentNum(){
+      this.game.currentNum ++;
+    }
+
     startGame() {
-      if (currentNum === parseInt(this.el.textContent)) {
-        this.el.classList.add("pushed");
-        currentNum++;
+      if (this.getCurrentNum() === parseInt(this.getEl().textContent)) {
+        this.getEl().classList.add("pushed");
+        this.addCurrentNum();
       }
-      if(currentNum === 4){
-        clearTimeout(timeoutId);
-        startBtn.classList.remove('pushed');
+      if (this.getCurrentNum() === this.game.getLevel() ** 2) {
+        clearTimeout(this.game.timeoutId);
+        this.game.startBtn.classList.remove("pushed");
       }
     }
   }
   //ここまでPanelクラス
 
   class Board {
-    constructor() {
-      this.panels = [new Panel(), new Panel(), new Panel(), new Panel()];
-
+    constructor(game) {
+      this.game = game;
+      this.panels = [];
+      for(let i = 0; i < this.game.getLevel() ** 2; i++){
+        this.panels.push(new Panel(this.game));
+      }
     }
 
-    showNumber(){
-      this.nums = [0, 1, 2, 3];
+    
 
+    showNumber() {
+      this.nums = [];
+      for(let i = 0; i < this.game.getLevel() ** 2; i ++){
+        this.nums.push(i);
+      }
       this.panels.forEach((panel) => {
         panel.el.textContent = this.nums.splice(
           Math.floor(Math.random() * this.nums.length),
@@ -46,29 +67,37 @@
   }
   //ここまでBoardクラス
 
-  function countUp(){
-    const timer = document.getElementById('timer');
-    timeoutId = setTimeout(() => {
-      timer.textContent = ((Date.now() - startTime)/1000).toFixed(2);
-      countUp();
-    },10);
+  class Game {
+    constructor(level) {
+      this.level = level;
+      this.currentNum = undefined;
+      this.startTime = undefined;
+      this.timeoutId = undefined;
+      const board = new Board(this);
 
+      this.startBtn = document.getElementById("start-btn");
+      this.startBtn.addEventListener("click", () => {
+        this.startBtn.classList.add("pushed");
+        this.currentNum = 0;
+        this.startTime = Date.now();
+        board.showNumber();
+        this.countUp();
+      });
+    }
+
+    getLevel(){
+      return this.level;
+    }
+
+    countUp() {
+      const timer = document.getElementById("timer");
+      this.timeoutId = setTimeout(() => {
+        timer.textContent = ((Date.now() - this.startTime) / 1000).toFixed(2);
+        this.countUp();
+      }, 10);
+    }
   }
+  //ここまでGameクラス
 
-  let currentNum;
-  let startTime;
-  let timeoutId;
-
-  const board = new Board()
-
-  const startBtn = document.getElementById("start-btn");
-  startBtn.addEventListener("click", () => {
-    startBtn.classList.add('pushed');
-    currentNum = 0;
-    startTime = Date.now()
-    board.showNumber();
-    countUp();
-  });
-
-
+  new Game(3);
 }
