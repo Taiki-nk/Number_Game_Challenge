31ae2df80e29c7302e20ea3ef521dc716b04a2e7
Taiki-nk <taikifate.0206@gmail.com>
2020-06-25 12:41:24 +0900

    tweet機能実装途中

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
       this.game = game;
       this.el = document.createElement("li");
       this.el.classList.add("pushed");
+      this.time = timer.textContent;
 
       const board = document.getElementById("board");
       board.appendChild(this.el);
@@ -14,6 +15,8 @@
         this.startGame();
       });
     }
+
+    
 
     getEl() {
       return this.el;
@@ -35,6 +38,10 @@
       if (this.getCurrentNum() === this.game.getLevel() ** 2) {
         clearTimeout(this.game.timeoutId);
         this.game.startBtn.classList.remove("pushed");
+        this.game.showScore();
+        this.time = timer.textContent;
+        console.log(this);
+        tweet.classList.add("active");
       }
     }
   }
@@ -76,6 +83,8 @@
       this.currentNum = undefined;
       this.startTime = undefined;
       this.timeoutId = undefined;
+
+      this.timer = document.getElementById("timer");
       const board = new Board(this);
 
       board.boardStyle();
@@ -84,6 +93,7 @@
       this.startBtn = document.getElementById("start-btn");
       this.startBtn.addEventListener("click", () => {
         this.startBtn.classList.add("pushed");
+        tweet.classList.remove("active");
         this.currentNum = 0;
         this.startTime = Date.now();
         board.showNumber();
@@ -95,36 +105,86 @@
       return this.level;
     }
 
-    changeTitle(){
-      const titleLevel = document.querySelector('.level');
+    changeTitle() {
+      const titleLevel = document.querySelector(".level");
       titleLevel.textContent = gameLevel;
     }
 
+    showScore() {
+      const coments = [
+        "まだ行けます！",
+        "これは早い！ …方だと思います",
+        "そろそろレベルを上げてみては？",
+        "おそらく最速です！ おそらくですよ？",
+        "人の領域を超えました",
+        "この反応速度、神かもしれない…",
+      ];
+      const scoreComent = document.getElementById("score");
+      if (gameLevel < 4) {
+        console.log("test");
+        if (this.timer.textContent < 10) {
+          scoreComent.textContent = coments[2];
+        } else {
+          scoreComent.textContent = coments[0];
+        }
+      } else {
+        if (this.timer.textContent < 8) {
+          scoreComent.textContent = coments[5];
+        } else if (this.timer.textContent < 10) {
+          scoreComent.textContent = coments[4];
+        } else if (this.timer.textContent < 12) {
+          scoreComent.textContent = coments[3];
+        } else if (this.timer.textContent < 15) {
+          scoreComent.textContent = coments[1];
+        } else {
+          scoreComent.textContent = coments[0];
+        }
+      }
+    }
+
     countUp() {
-      const timer = document.getElementById("timer");
       this.timeoutId = setTimeout(() => {
-        timer.textContent = ((Date.now() - this.startTime) / 1000).toFixed(2);
+        this.timer.textContent = ((Date.now() - this.startTime) / 1000).toFixed(
+          2
+        );
         this.countUp();
       }, 10);
     }
   }
   //ここまでGameクラス
 
-  let gameLevel ;
+  let gameLevel;
+  // let time = game.timer.textContent;
+
+
   function startGame() {
     gameLevel = prompt("ゲームレベルを数字の2〜4の中で入力してください");
-    if (!parseInt(gameLevel) || gameLevel === "" || gameLevel === null || parseInt(gameLevel) < 2 || parseInt(gameLevel) > 5) {
-      console.log(gameLevel)
+    if (
+      !parseInt(gameLevel) ||
+      gameLevel === "" ||
+      gameLevel === null ||
+      parseInt(gameLevel) < 2 ||
+      parseInt(gameLevel) > 4
+    ) {
+      console.log(gameLevel);
       alert("もう一度入力してください");
       startGame();
     } else {
-      if(parseInt(gameLevel) !==NaN ){
+      if (parseInt(gameLevel) !== NaN) {
         new Game(gameLevel);
-      } 
+      }
     }
-    
-    
   }
 
+
+  function tweetHref() {
+    tweet.href = `https://twitter.com/intent/tweet?text=ゲームレベル${gameLevel}でスコアは${time}秒でした`;
+  };
+
   startGame();
+
+  this.tweet = document.getElementById("tweet");
+  tweet.addEventListener("click", () => {
+    tweetHref();
+  });
 }
